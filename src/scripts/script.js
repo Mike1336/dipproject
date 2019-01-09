feb12521bed2d04233cd4cadb6c0e9727430c5c3
Mike1336 <muzhikovm@bk.ru>
2019-01-09 17:04:07 +0300

    changed content selection code and added selected content in cookie

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,36 +7,116 @@ employeeContent = document.getElementById('employee');
 preloader = document.getElementById('page-preloader');
 
 
-document.body.onload = function(){
-    showInventory();
-        setTimeout(function(){
-            if ( !preloader.classList.contains('done') ) 
-            {
-                preloader.classList.add('done');
-            } 
-        }, 1000);
+    document.body.onload = function(){
+        if(document.cookie=="content=inventory")
+        {
+        selectContent("inventory");
+        }
+        else if(document.cookie=="content=employee")
+        {
+        selectContent("employee");
+        }
+        else
+        {
+            selectContent("inventory");
+        }
+            setTimeout(function(){
+                if ( !preloader.classList.contains('done') ) 
+                {
+                    preloader.classList.add('done');
+                } 
+            }, 1000);
 
 } 
 
-    inventorySelect.onclick = showInventory;
+        inventorySelect.onclick = function(){
+            deleteCookie("content");
+            selectContent("inventory");
+            setCookie("content", "inventory", {
+                expires: 0 //куки будут храниться до закрытия браузера
+                })
 
-    employeeSelect.onclick = showEmployee;
+        }
+        employeeSelect.onclick = function(){
+            deleteCookie("content");  
+            selectContent("employee");
+            setCookie("content", "employee", {
+                expires: 0 //куки будут храниться до закрытия браузера
+                })
+        }
 
-        function showInventory() {
-            document.title ="Инвентарные единицы | Главная страница";
+            function selectContent(content) { //функция выбора контента
 
-            inventorySelect.style.background="#578530";
-            employeeSelect.style.background="#fff";
-            inventoryContent.style.display="grid";
+            if(content == "inventory") 
+            {
+                if(!inventorySelect.classList.contains('active')) 
+                {
+                    inventorySelect.classList.add('active');
+                    employeeSelect.classList.remove('active');
+                    showContent("inventory"); 
+                }
 
-            employeeContent.style.display="none";
-        };
-        function showEmployee() {
-            document.title ="Сотрудники | Главная страница";
+            }
 
-            inventorySelect.style.background="#fff";
-            employeeSelect.style.background="#578530";
+            else if (content == "employee") 
+            {
+                if ( !employeeSelect.classList.contains('active') ) 
+                {
+                    employeeSelect.classList.add('active');
+                    inventorySelect.classList.remove('active');
+                    showContent("employee");
+                }
 
-            employeeContent.style.display="grid";
-            inventoryContent.style.display="none";
-        };
+            }
+
+            };
+            function showContent(content){ //функция показа контента
+            if(content == "inventory")
+            {
+                document.title ="Инвентарные единицы | Главная страница"
+                inventoryContent.style.display="grid"
+                employeeContent.style.display="none";
+            }
+
+            else if(content == "employee")
+            {
+                document.title ="Сотрудники | Главная страница"
+                employeeContent.style.display="grid"
+                inventoryContent.style.display="none";
+            }
+            }
+
+    function setCookie(name, value, options) { //функция добавления в куки выбранного контента
+        options = options || {};
+        
+        var expires = options.expires;
+        
+        if (typeof expires == "number" && expires) {
+            var d = new Date();
+            d.setTime(d.getTime() + expires * 1000);
+            expires = options.expires = d;
+        }
+        if (expires && expires.toUTCString) {
+            options.expires = expires.toUTCString();
+        }
+        
+        value = encodeURIComponent(value);
+        
+        var updatedCookie = name + "=" + value;
+        
+        for (var propName in options) {
+            updatedCookie += "; " + propName;
+            var propValue = options[propName];
+            if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+            }
+        }
+        
+        document.cookie = updatedCookie;
+        }
+
+        function deleteCookie(name) {//функция удаления из куки ранее выбранного контента
+            setCookie(name, "", {
+            expires: -1
+            })
+        }
